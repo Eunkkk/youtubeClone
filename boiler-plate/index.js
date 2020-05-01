e4d9d9cb3430ae61b94b91eb078b69ab34eb3481
EUN-KYU-BAEK <dmsrb1595@naver.com>
2020-05-01 19:25:46 +0900

    test

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:admin@youtubeclone-jos0f.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://admin:admin@cluster0-r7hqg.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
